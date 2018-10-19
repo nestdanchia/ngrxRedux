45d56f1c5931c27b1f0a3dcfb6f33324b49348fe
Nestor <chiariellonestordaniel@gmail.com>
2018-10-19 18:25:40 -0300

    angular 6

--- src/app/views/about/about.component.ts
+++ src/app/views/about/about.component.ts
@@ -3,7 +3,7 @@ import { Component, OnInit } from "@angular/core";
 @Component({
   selector: "cf-about",
   template: `
-  <h3><a href="http://www.linkedin.com/in/nestordanielchiariello">  Angular 5 Tutorial</a></h3>
+  <h3><a href="http://www.linkedin.com/in/nestordanielchiariello">  Angular 6 Tutorial ngrx</a></h3>
   <blockquote>
   <p><em>Learn, Code, Enjoy, Repeat.</em></p>
 </blockquote>
